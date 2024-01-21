f4a9cb8d43acf95deffa0ac7c7505dd4c30cbebb
Jun Suzuki <junsuzuki@gmx.fr>
2024-01-21 18:22:40 +0100

    Added comments in snake.go

--- game/snake.go
+++ game/snake.go
@@ -1,11 +1,14 @@
 package game
 
+// Define the snake with a slice of x,y coordinates for its body
 type Snake struct {
-	Body [][2]float32 // Array of x, y pairs
+	Body [][2]float32
 }
 
+// Possible directions the snake can move in
 type Direction int
 
+// Enumeration of directions
 const (
 	DirUp Direction = iota
 	DirDown
@@ -13,6 +16,7 @@ const (
 	DirRight
 )
 
+// Check if the given direction is opposite to the current direction
 func (d Direction) IsOpposite(other Direction) bool {
 	switch d {
 	case DirUp:
@@ -27,6 +31,7 @@ func (d Direction) IsOpposite(other Direction) bool {
 	return false
 }
 
+// Return the unit vector (as x, y increments) for the given direction
 func (d Direction) Vector() (int, int) {
 	switch d {
 	case DirUp:
@@ -41,12 +46,14 @@ func (d Direction) Vector() (int, int) {
 	return 0, 0
 }
 
+// Initialize and return a new snake with a default length and position
 func NewSnake() Snake {
 	body := make([][2]float32, int(InitialSnakeLength))
+
+	// Set the initial position of the snake's head and align the body towards the left
 	startX := (PlayAreaX1 / SnakeSize) + (PlayAreaX1 * 3 / SnakeSize)
 	startY := (PlayAreaY1 / SnakeSize) + (PlayAreaY1 * 4 / SnakeSize)
 
-	// Initialize the snake's body towards the left and at the center
 	for i := 0; i < int(InitialSnakeLength); i++ {
 		body[i] = [2]float32{startX + float32(i), startY}
 	}
